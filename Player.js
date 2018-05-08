711e1b1a6f63f4883ef39a10c6b4202d9ba85970
Robert Willemelis <robert.dev@gmx.de>
2018-05-08 13:40:16 +0200

    console.log myCard

--- Player.js
+++ Player.js
@@ -41,6 +41,7 @@ class Player {
       console.log(JSON.stringify(communityCards));
 
       myCards.forEach(myCard => {
+        console.log(JSON.stringify(myCard));
         myCard.forEach(card => {
           console.log('myCard: ' + card);
         })

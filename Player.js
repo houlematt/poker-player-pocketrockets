a190fda99cb588717efa0df1c35ef6663824d1b8
mhoule <matt.houle@meltwater.com>
2018-05-08 05:26:21 -0400

    go all in

--- Player.js
+++ Player.js
@@ -3,18 +3,39 @@ class Player {
     return '0.1';
   }
 
+
+
+  static isPair(cards){
+    if(cards.length==2){
+      if(cards[0].rank == cards[1].rank){
+        return true;
+      }
+    }
+    return false;
+  }
+
   static betRequest(gameState, bet) {
 
     console.log(gameState);
-    bet(gameState.current_buy_in + 10);
 
-    let myCards = '';
+    let myCards = '',
+        myStack = 0;
     for (var step = 0; step < 4; step++) {
       if (gameState.players[step].name==='PocketRockets') {
         myCards = gameState.players[step].hole_cards;
+        myStack = gameState.players[step].stack;
         console.info('myCards', myCards);
       }
     }
+
+    let howMuchShouldIBet = 0;
+
+    if(this.isPair(myCards)){
+      howMuchShouldIBet = myStack;
+    }
+
+
+    bet(howMuchShouldIBet);
 
   }
 

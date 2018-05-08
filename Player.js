2b55f4b1cb6c5785bc220713bf53052c826f489d
Robert Willemelis <robert.dev@gmx.de>
2018-05-08 11:42:05 +0200

    add try catch

--- Player.js
+++ Player.js
@@ -29,6 +29,10 @@ class Player {
 
   static betRequest(gameState, bet) {
 
+    try{
+
+    
+
     console.log(gameState);
 
     let myCards = '',
@@ -52,6 +56,11 @@ class Player {
     }
 
     bet(howMuchShouldIBet);
+
+  } catch(error){
+    console.log('we have an error: ' + error);
+    bet(10);
+  }
 
   }
 

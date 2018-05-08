a838810426960fc6379c0e69a163cd08fd81809c
Seid Zahirovic <szahirovic@northernsafety.com>
2018-05-08 16:24:44 +0200

    Added haveStraightFlush method

--- Player.js
+++ Player.js
@@ -5,8 +5,6 @@ class Player {
     return '0.1';
   }
 
-
-
   static isPair(cards){
     if(cards.length==2){
       if(cards[0].rank == cards[1].rank){
@@ -23,6 +21,33 @@ class Player {
       }
     }
     return false;
+  }
+  // remember gameState
+  static writeGame(gameState){
+    try{
+
+    
+    var request = require("request");
+
+// var options = { 
+//     method: 'POST',
+//     url: 'https://poker-1721.restdb.io/rest/games',
+//     headers: 
+//     {   'cache-control': 'no-cache',
+//         'x-apikey': 'fdbae10e99aa9814474124823a0a058cb9973',
+//         'content-type': 'application/json' },
+//     body: { game_id: gameState.game_id, rounds: '1' },
+//     json: true 
+// };
+
+// request(options, function (error, response, body) {
+//     if (error) throw new Error(error);
+//     console.log(body);
+// });
+    } catch(error){
+      console.log('writeGame: ' + error)
+
+    }
   }
 
   static haveStraight(communityCards, myCards) {
@@ -53,6 +78,12 @@ class Player {
     return false;
   }
 
+  static haveStraightFlush(communityCards, myCards) {
+
+    if (haveStraight(communityCards, myCards) && haveFlush(communityCards, myCards)) {      return true;    }
+
+    return false;  }
+
   static haveAnyPair(communityCards, myCards){
     for (let i = 0; i < communityCards.length; i++) {
       let cCard = communityCards[i];
@@ -62,6 +93,90 @@ class Player {
           return true;
         }
       }
+    }
+    return false;
+  }
+
+  static haveFlush(communityCards, myCards){
+    let hasFlush = false;
+    try{
+      function countInArray(array, value) {
+        return array.reduce((n, x) => n + (x === value), 0);
+      }
+      console.log('-- haveFlush --');
+      console.log(JSON.stringify(myCards));
+      console.log(JSON.stringify(communityCards));
+      let suitsMyCards = [];
+      let suitsCommunitycards = [];
+      let myCardSuitesEqual = false;
+
+      myCards.forEach(card => {
+        suitsMyCards.push(card.suit);
+        console.log('my card: ' + JSON.stringify(card));
+      });
+      communityCards.forEach(card => {
+        suitsCommunitycards.push(card.suit);
+        console.log('community card: ' + JSON.stringify(card));
+      });
+      if(suitsMyCards[0] == suitsMyCards[1]){
+        myCardSuitesEqual = true;
+      }
+      if(suitsCommunitycards.length >=3 && myCardSuitesEqual){
+        console.log('myCards equal suites');
+        suitsMyCards.forEach(suit => {
+          let numSuits = 0;
+          numSuits = countInArray(suitsCommunitycards, suit);
+          console.log('suit: '+ suit + ' occurs ' + numSuits + ' times in community cards' )
+          if(numSuits >=3){
+            console.log('### flush');
+            hasFlush = true;
+          }
+        });
+      } else {
+        console.log('myCards no equal suites');
+        suitsMyCards.forEach(suit => {
+          let numSuits = 0;
+          numSuits = countInArray(suitsCommunitycards, suit);
+          console.log('suit: '+ suit + ' occurs ' + numSuits + ' times in community cards' )
+          if(numSuits >=4){
+            console.log('### flush');
+            hasFlush = true;
+          }
+        });
+
+      }
+      return hasFlush;
+
+    } catch(error){
+      console.log('has flush error: '+ error);
+    }
+    return false;
+  }
+
+  static highPair(myCards){
+    let valuableCards = ["10","J","Q","K","A"];
+    if(valuableCards.indexOf(myCards[0].rank) != -1){
+      console.log('high pair');
+      return true;
+    }
+    console.log('not high pair');
+    return false;
+  }
+
+  static highCard(myCards){
+      let valuableCards = ["J","Q","K","A"];
+      for (let i = 0; i < myCards.length; i++) {
+        if(valuableCards.indexOf(myCards[0].rank) != -1){
+          console.log('high single');
+          return true;
+        }
+      }
+      return false;
+  }
+
+  static isPreFlop(community_cards){
+    if (community_cards.length == 0){
+      return true;
     }
     return false;
   }
@@ -84,22 +199,41 @@ class Player {
 
     let howMuchShouldIBet = 0;
 
-    if(this.isPair(myCards)){
-      console.log('isPair found');
-      howMuchShouldIBet = gameState.minimum_raise;
-    }
-    
-    if(this.isSameSuit(myCards)){
-      console.log('isSameSuit found');
-      howMuchShouldIBet = gameState.minimum_raise;
-    }
+    // if(gameState.minimum_raise > 0)
 
-    if(this.haveAnyPair(gameState.community_cards, myCards)){
+     if(this.isPair(myCards)){
+      if(this.highPair(myCards)&& this.isPreFlop(gameState.community_cards)){
+        howMuchShouldIBet = myStack/8;
+      }else if (this.isPreFlop(gameState.community_cards)){
+        howMuchShouldIBet = myStack / 10;
+      }
+    } else if(this.isPreFlop(gameState.community_cards) && highCard(myCards) && gameState.bet_index < 10){
+        if(gameState.minimum_raise < 100){
+          howMuchShouldIBet = gameState.minimum_raise * 2;
+        }
+    } else if(this.isSameSuit(myCards) && this.isPreFlop(gameState.community_cards)&& gameState.bet_index < 10){
+
+      console.log('isSameSuit found');
+      if(gameState.minimum_raise < 100){
+        howMuchShouldIBet = gameState.minimum_raise * 2;
+      }
+    } else if(this.haveAnyPair(gameState.community_cards, myCards)){
       console.log('haveAnyPair found');
       howMuchShouldIBet = gameState.minimum_raise;
     }
 
-    bet(howMuchShouldIBet);
+
+    console.log('\r\n \r\n', howMuchShouldIBet)
+
+    // if(!this.isPreFlop(gameState.community_cards)){
+    //   if(gameState.in_action > 0){
+
+    //   }
+    // }
+
+    this.haveFlush(gameState.community_cards, myCards);
+    this.writeGame(gameState);
+        bet(Math.round(howMuchShouldIBet));
 
   } catch(error){
     console.log('we have an error: ' + error);
